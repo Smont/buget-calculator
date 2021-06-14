ab9c9c954d3c3397a8906f1488a6db50c6fd5f04
Smont <montagnani.si@gmail.com>
2021-06-13 21:37:39 -0300

    finished design-ui implemetation

--- src/app/buget-item-list/buget-item-card/buget-item-card.component.ts
+++ src/app/buget-item-list/buget-item-card/buget-item-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-buget-item-card',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./buget-item-card.component.scss']
 })
 export class BugetItemCardComponent implements OnInit {
+
+  @Input() isIncome: boolean = true;
 
   constructor() { }
 
